f374375143365668e8f1c8b7ca65fd65171fb248
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-02-10 01:06:45 +0200

    fixed urls and params

--- front-end/src/scripts/index.js
+++ front-end/src/scripts/index.js
@@ -1,58 +1,60 @@
+let base_url = 'http://localhost/project1/back-end/'
+
 const getEquation = () => {
     const id ='equation'
-    const url = ''
     let x = document.getElementById('x').value
     let y = document.getElementById('y').value
+    let url = `${base_url}equation.php?x=${x}&y=${y}`
     fetch(new URL(url))
     .then(res => res.json())
     .then(json => {
-        document.querySelector(`${id} .results`).textContent = `Result: ${json.result}`
+        document.querySelector(`#${id} .results`).textContent = `Result: ${json.result}`
     })
     .catch(err => showErr(id, err))
 }
 
 const checkPalindrome = () => {
-    const id = 'palindrome-input'
-    const url = ''
-    let input = document.getElementById(id).value
+    const id = 'palindrome'
+    let input = document.querySelector(`#${id} input`).value
+    let url = `${base_url}palindrome.php?string=${input}`
     fetch(new URL(url))
     .then(res => res.json())
     .then(json => {
         let msg = json.isPalindrome ? 'it\'s a Palindrome!' : 'This is NOT a Palindrome!!!'
-        document.querySelector(`${id} .results`).textContent = `Result: ${msg}`
+        document.querySelector(`#${id} .results`).textContent = `Result: ${msg}`
     })
     .catch(err => showErr(id, err))
 }
 
 const trimSpaces = () => {
-    const id = 'spaces-input'
-    const url = ""
-    let input = document.getElementById(id).value
+    const id = 'spaces'
+    let input = document.querySelector(`#${id} input`).value
+    let url = `${base_url}spaces.php?string=${input}`
     fetch(new URL(url))
     .then(res => res.json())
     .then(json => {
-        document.querySelector(`${id} .results`).textContent = `Result: ${json.cleanedString}`
+        document.querySelector(`#${id} .results`).textContent = `Result: ${json.cleanedString}`
     })
     .catch(err => showErr(id, err))
 }
 
 const validatePass = () => {
-    const id = 'pass-input'
-    const url = ""
-    let input = document.getElementById(id).value
+    const id = 'password'
+    let input = document.querySelector(`#${id} input`).value
+    let url = `${base_url}password.php?password=${input}`
     fetch(new URL(url))
     .then(res => res.json())
     .then(json => {
         let is_valid = json.isValid ? 'Valid Password' : 'this Password is not valid'
         let msg = `Result: <br/> ${is_valid} <br/> harsh: "${json.hash}"`
-        document.querySelector(`${id} .results`).innerHTML = msg
+        document.querySelector(`#${id} .results`).innerHTML = msg
     })
     .catch(err => showErr(id, err))
 }
 
 const showErr = (id, error) => {
     let msg = `An error has occured: ${error}`
-    document.querySelector(`${id} .results`).textContent = msg
+    document.querySelector(`#${id} p.results`).textContent = msg
 }
 
 const main = () => {
